8e818f1ceedddcc6abfbe13c07c83cda4808c37a
Tanner <tdyer@hawk.iit.edu>
2020-09-10 10:39:51 -0500

    implemented textView labels changing based upon the selected radio button

--- app/src/main/java/com/example/distanceconverter/MainActivity.java
+++ app/src/main/java/com/example/distanceconverter/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.distanceconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView inputTextView;
+    private TextView outputTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputTextView = findViewById(R.id.inputValueTextView);
+        outputTextView = findViewById(R.id.outputValueTextView);
+    }
+
+    public void radioSelected(View v) {
+        switch (v.getId()) {
+            case R.id.radioMiletoKm:
+                inputTextView.setText(R.string.milesTextView);
+                outputTextView.setText(R.string.kilometersTextView);
+                break;
+            case R.id.radioKmToMile:
+                inputTextView.setText(R.string.kilometersTextView);
+                outputTextView.setText(R.string.milesTextView);
+                break;
+        }
     }
 }
